02ed1f01dbfcd57252c0f21fbcc68186004a4edb
Michael Peterson <michaeljoepeterson@gmail.com>
2019-09-23 19:57:59 -0600

    added cors

--- public/js/interfacer.js
+++ public/js/interfacer.js
@@ -50,7 +50,7 @@ Interfacer.prototype.postReport = function(event) {
 	};
 	let settings = {
 		method:'POST',
-		url:'/api/reports',
+		url:'https://bambora-test.herokuapp.com//api/reports',
 		data:JSON.stringify(reportOptions),
 		contentType: 'application/json'
 
--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const jsonParser = bodyParser.json();
 const {router: reportsRouter} = require('./routers/reports');
 const app = express();
 app.use(express.static('public'));
-/*
+
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization,Accept');
@@ -17,7 +17,7 @@ app.use(function (req, res, next) {
   }
   next();
 });
-*/
+
 app.use(jsonParser);
 app.use('/api/reports',reportsRouter);
 
